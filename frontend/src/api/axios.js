8452c5fb15c6767b76d084aa94b42652eccc3037
vijayaragavaragan7-tech <vijayaragavaragavan7@gmail.com>
2026-07-09 09:57:43 +0530

    Fix backend URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ineedblood-backend.onrender.com/api",
 });
 
 // Attach token automatically if user is logged in
